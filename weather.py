06033a6054d4474ba3a1b46a9dd66dbe7302e977
Bryce Etheridge <cbetheridge@gmail.com>
2014-04-30 22:01:35 -0700

    implimenting gameboard, a very basic weather module.

---
+++ weather.py
@@ -0,0 +1,42 @@
+import random
+import resource
+
+
+class AbstractWeather(object):
+	_region = ""
+	_biome = ""
+
+	def __init__(self, region, biome):
+		self._region = region
+		self._biome = _biome
+
+	def run(self):
+		"""Accepts reference to weather's owning gridsquare and runs rules.
+
+		This method should be overridden by subclasses.
+		"""
+		pass
+
+class RaineyWeather(AbstractWeather):
+	"""Impliments Rain weather conditions.
+
+	Rain is a weather condition where the game square has a random chance
+	of generating a resource every turn and a random chance that the square
+	will 'dry up' each turn, removing resources.
+	"""
+
+	def __init__(self, region, biome):
+		super(raineyWeather, self).__init__(region, biome)
+
+	@override
+	def run(self):
+		"""Impliments weather rules."""
+		rain_chance = random.randint(1,4)
+
+		if rain_chance <= 1:
+			#rain
+			self.addResource(resource('basic', 1))
+		elif rain_chance >= 4:
+			#dry up
+			self.removeResource(resource('basic', 1))
+
